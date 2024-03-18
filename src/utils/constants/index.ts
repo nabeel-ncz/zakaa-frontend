0852065f7888aaca83cd1e390e1d5b0fb6dbbebf
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-03-18 15:03:09 +0530

    prod: updated backend urls

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,2 +1,2 @@
-export const PUBLIC_RESOURCE_URL = 'http://localhost:5000';
-export const SOCKET_SERVICE = 'http://localhost:3007';
+export const PUBLIC_RESOURCE_URL = 'http://zakaa.nabeel.cloud';
+export const SOCKET_SERVICE = 'http://zakaa.nabeel.cloud';

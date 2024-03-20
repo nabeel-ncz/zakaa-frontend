370cf738486348d6c768ab2b6aab4516f6c864e7
nabeel-ncz <nabeelnabhan115@gmail.com>
2024-03-20 17:04:28 +0530

    fix: constants changed

--- src/utils/constants/index.ts
+++ src/utils/constants/index.ts
@@ -1,2 +1,2 @@
 export const PUBLIC_RESOURCE_URL = 'https://zakaa.nabeel.cloud';
-export const SOCKET_SERVICE = 'https://zakaa.nabeel.cloud/api/chat';
+export const SOCKET_SERVICE = 'https://zakaa.nabeel.cloud';
